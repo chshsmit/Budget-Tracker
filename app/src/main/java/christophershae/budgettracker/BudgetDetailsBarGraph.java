0c3466a599a7e422696b84ab5b76d04df2b1a81a
zneakiv <zack.neak@gmail.com>
2017-10-22 04:28:28 -0700

    first commit

---
+++ app/src/main/java/christophershae/budgettracker/BudgetDetailsBarGraph.java
@@ -0,0 +1,13 @@
+package christophershae.budgettracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BudgetDetailsBarGraph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_details_bar_graph);
+    }
+}
