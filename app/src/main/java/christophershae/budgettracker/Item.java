5dc1d58b80086fd9e3685a03ab12590fe236c319
Christopher Smith <chshsmit@ucsc.edu>
2017-10-26 14:42:13 -0700

    Adding created item class to item branch

---
+++ app/src/main/java/christophershae/budgettracker/Item.java
@@ -0,0 +1,50 @@
+package christophershae.budgettracker;
+
+/**
+ * Created by chrissmith on 10/26/17.
+ */
+
+public class Item {
+
+    public String name;
+    public String category;
+    public double price;
+
+    public String storeName;
+
+
+    //Here is our constructed method
+    public Item(String name){
+        this.name = name;
+    }
+
+    //---------------------------------------------------------------------------------------------
+    //These are all setter and getter functions
+    //---------------------------------------------------------------------------------------------
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public void setStoreName(String storeName){
+        this.storeName = storeName;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public String getCategory(){
+        return this.category;
+    }
+
+    public String getStoreName(){
+        return this.storeName;
+    }
+
+
+}
