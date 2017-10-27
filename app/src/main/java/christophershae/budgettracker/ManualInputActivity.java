6d11f0dd400ab9e4ad0b3d967db7f628d7552458
Christopher Smith <chshsmit@ucsc.edu>
2017-10-27 13:29:36 -0700

    Items can now be added to the list view on the manual input activity

--- app/src/main/java/christophershae/budgettracker/ManualInputActivity.java
+++ app/src/main/java/christophershae/budgettracker/ManualInputActivity.java
@@ -1,13 +1,138 @@
 package christophershae.budgettracker;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static christophershae.budgettracker.R.id.item;
+import static christophershae.budgettracker.R.id.itemNameView;
+import static java.security.AccessController.getContext;
 
 public class ManualInputActivity extends AppCompatActivity {
+
+
+
+    //----------------------------------------------------------------------------------------
+    //This code handles the generation of the list view
+    //----------------------------------------------------------------------------------------
+
+    private class ListElement {
+        ListElement() {};
+
+        ListElement(String nl, String cl) {
+            nameLabel = nl;
+            priceLabel = cl;
+        }
+
+        public String nameLabel;
+        public String priceLabel;
+    }
+
+
+
+    private class MyAdapter extends ArrayAdapter<ListElement> {
+
+        int resource;
+        Context context;
+
+        public MyAdapter(Context _context, int _resource, List<ListElement> items) {
+            super(_context, _resource, items);
+            resource = _resource;
+            context = _context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout newView;
+
+            ListElement currentItem = getItem(position);
+
+            // Inflate a new view if necessary.
+            if (convertView == null) {
+                newView = new LinearLayout(getContext());
+                LayoutInflater vi = (LayoutInflater)
+                        getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                vi.inflate(resource,  newView, true);
+            } else {
+                newView = (LinearLayout) convertView;
+            }
+
+            // Fills in the view.
+            TextView itemName = (TextView) newView.findViewById(itemNameView);
+            TextView itemCategory = (TextView) newView.findViewById(R.id.itemCategory);
+            TextView itemPrice = (TextView) newView.findViewById(R.id.itemPriceView);
+
+
+            itemName.setText(currentItem.nameLabel);
+            itemPrice.setText(currentItem.priceLabel);
+            itemCategory.setText("Generic");
+
+
+
+            // Set a listener for the whole list item.
+            newView.setTag(currentItem.nameLabel);
+            newView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String s = v.getTag().toString();
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, s, duration);
+                    toast.show();
+                }
+            });
+
+            return newView;
+        }
+    }
+
+    private MyAdapter aa;
+    private ArrayList<ListElement> currentItemsAddedToList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_input);
+        currentItemsAddedToList = new ArrayList<ListElement>();
+        aa = new MyAdapter(this, R.layout.manually_input_list_element, currentItemsAddedToList);
+        ListView myListView = (ListView) findViewById(R.id.itemsAddedToBudgetAlready);
+        myListView.setAdapter(aa);
+        aa.notifyDataSetChanged();
     }
+
+
+    public String newItemName;
+    public String newItemPrice;
+
+
+    public void createNewItem(View v){
+        EditText nameEntry = (EditText) findViewById(R.id.itemNameEntry);
+        EditText priceEntry = (EditText) findViewById(R.id.itemPriceEntry);
+
+        newItemPrice = priceEntry.getText().toString();
+        newItemName = nameEntry.getText().toString();
+
+
+        Item newItem = new Item(newItemName);
+        newItem.setPrice(Float.valueOf(newItemPrice));
+        newItem.setCategory("Generic");
+
+        currentItemsAddedToList.add(new ListElement(newItemName, newItemPrice));
+        aa.notifyDataSetChanged();
+    }
+
+
+
 }
