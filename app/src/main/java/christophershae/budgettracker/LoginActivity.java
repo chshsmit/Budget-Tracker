efba1d4e287c2354f1fe98dbcc08ae321cf01c25
Darwin-Li <32622886+Darwin-Li@users.noreply.github.com>
2017-11-03 21:35:47 -0700

    SignUp Working

--- app/src/main/java/christophershae/budgettracker/LoginActivity.java
+++ app/src/main/java/christophershae/budgettracker/LoginActivity.java
@@ -30,16 +30,21 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements OnClickListener {
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -52,6 +57,9 @@ public class LoginActivity extends AppCompatActivity {
      *
      */
     private FirebaseAuth firebaseAuth;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonSignup;
 
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
@@ -70,12 +78,42 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         firebaseAuth = FirebaseAuth.getInstance();
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        buttonSignup = (Button) findViewById(R.id.signUp);
+
+        buttonSignup.setOnClickListener(this);
+
+        System.out.println(editTextEmail);
+        System.out.println(editTextPassword);
 
     }
     public void Simple_Nav(View view){
         Intent next_activity = new Intent(LoginActivity.this, MainBudgetScreen.class);
         startActivity(next_activity);
+    }
+
+    private void registerUser()
+    {
+
+        //getting email and password from edit texts
+        String email = editTextEmail.getText().toString().trim();
+        String password  = editTextPassword.getText().toString().trim();
+
+
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password);
+
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        //calling register method on click
+        registerUser();
     }
 
 }
