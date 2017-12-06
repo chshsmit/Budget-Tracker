d6e643ee76eb09ef826def1c9ddec8b495782034
Nathan McHone <nlhutchi@ucsc.edu>
2017-12-06 14:45:53 -0800

    commenting updates

--- app/src/main/java/christophershae/budgettracker/about.java
+++ app/src/main/java/christophershae/budgettracker/about.java
@@ -6,7 +6,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class about extends AppCompatActivity {
+public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
