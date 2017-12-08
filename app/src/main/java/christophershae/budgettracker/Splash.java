c48dd4588f17b528ed71015a46abfba7691239eb
Darwin-LI <dli39@ucsc.edu>
2017-12-08 14:29:35 -0800

    commented splash classes

--- app/src/main/java/christophershae/budgettracker/Splash.java
+++ app/src/main/java/christophershae/budgettracker/Splash.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+
+//splash for coldstarting but login
 public class Splash extends AppCompatActivity {
 
     @Override
