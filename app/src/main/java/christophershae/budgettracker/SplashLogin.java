0f74e5462bdb4a0b94e373b8410ea0b21d45a013
zneakiv <zack.neak@gmail.com>
2017-12-08 14:38:24 -0800

    Extension of last commit

--- app/src/main/java/christophershae/budgettracker/SplashLogin.java
+++ app/src/main/java/christophershae/budgettracker/SplashLogin.java
@@ -5,11 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+//splash for coldstart without logged in
 public class SplashLogin extends AppCompatActivity
 {
-//splash for coldstart without logged in
-public class SplashLogin extends AppCompatActivity {
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
